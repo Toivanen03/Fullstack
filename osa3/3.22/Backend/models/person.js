c1c70dfc2b1bc3f29d16885be63a4195cc815bf6
Simo Toivanen <posti@simotoivanen.fi>
2024-11-22 11:10:15 +0200

    uibuild

---
+++ osa3/3.22/Backend/models/person.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+mongoose.connect(url)
+
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3,
+    required: [true, 'Nimi on pakollinen!']
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d{7}$/.test(v)
+      },
+      message: props => `${props.value} ei ole kelvollinen numero.`
+    },
+    required: [true, 'Puhelinnumero vaaditaan!']
+  }
+})
+
+contactSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  //  delete returnedObject.id
+  }
+})
+
+
+module.exports = mongoose.model('Person', contactSchema)
