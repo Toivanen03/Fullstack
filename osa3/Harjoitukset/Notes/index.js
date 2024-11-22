91a95d4022203f21557a430a13959bcd8d7c01eb
Simo Toivanen <posti@simotoivanen.fi>
2024-11-22 06:57:08 +0200

    Harjoituskansion päivitys

--- osa3/Harjoitukset/Notes/index.js
+++ osa3/Harjoitukset/Notes/index.js
@@ -51,44 +51,30 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
   
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
   
   const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
   })
   
-  note.save().then(savedNote => {
-  response.json(savedNote)
+  note.save()
+  .then(savedNote => {
+    response.json(savedNote)
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  const { content, important } = request.body
+  Note.findByIdAndUpdate(request.params.id, { content, important },
+    { new: true, runValidators: true, context: 'query' })
     .then(updatedNote => {
       response.json(updatedNote)
     })
     .catch(error => next(error))
 })
-
-const generateId = () => {
-  const maxId = Note.find({}).length > 0
-    ? Math.max(maxId(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
@@ -101,6 +87,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
   next(error)
 }
