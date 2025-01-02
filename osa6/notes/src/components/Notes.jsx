e03099d32a69210ef0d89e0a9dbccdfbb8634019
Simo Toivanen <posti@simotoivanen.fi>
2025-01-02 13:05:10 +0200

    6.3 - 6.8 palautukset

--- osa6/notes/src/components/Notes.jsx
+++ osa6/notes/src/components/Notes.jsx
@@ -1,30 +1,25 @@
-import { useDispatch, useSelector } from 'react-redux'
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { toggleImportanceOf } from '../reducers/noteReducer'
-
-const Note = ({ note, handleClick }) => {
-  return(
-    <li onClick={handleClick}>
-      {note.content} 
-      <strong> {note.important ? 'important' : ''}</strong>
-    </li>
-  )
-}
 
 const Notes = () => {
   const dispatch = useDispatch()
   const notes = useSelector(state => state)
 
-  return(
+  const toggleImportance = (id) => {
+    dispatch(toggleImportanceOf(id))
+  }
+
+  return (
     <ul>
-      {notes.map(note =>
-        <Note
-          key={note.id}
-          note={note}
-          handleClick={() => 
-            dispatch(toggleImportanceOf(note.id))
-          }
-        />
-      )}
+      {notes.map(note => (
+        <li key={note.id}>
+          {note.content} <strong>{note.important}</strong>
+          <button onClick={() => toggleImportance(note.id)}>
+            {note.important ? 'Make not important' : 'Make important'}
+          </button>
+        </li>
+      ))}
     </ul>
   )
 }
--- osa6/notes/src/index.jsx
+++ osa6/notes/src/index.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-import { createStore } from 'redux'
+import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-
+import store from './store'
 import App from './App'
-import noteReducer from './reducers/noteReducer'
 
-const store = createStore(noteReducer)
+const container = document.getElementById('root')
+const root = createRoot(container)
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+root.render(
   <Provider store={store}>
     <App />
   </Provider>
