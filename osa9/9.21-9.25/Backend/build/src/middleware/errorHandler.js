8d88f3b3697cf0a3663af690a28ef8ef908b6dc9
Simo Toivanen <posti@simotoivanen.fi>
2025-04-03 10:09:15 +0300

    10.1

--- osa9/9.21-9.25/Backend/build/src/middleware/errorHandler.js
+++ osa9/9.21-9.25/Backend/build/src/middleware/errorHandler.js
@@ -1,15 +1,21 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorMiddleware = void 0;
-const zod_1 = require("zod");
-const errorMiddleware = (error, _req, res, next) => {
-    if (error instanceof zod_1.z.ZodError) {
-        const messages = error.errors.map((err) => err.message).join('\n');
-        res.status(400).json(messages);
-    }
-    else {
-        res.status(500).json({ error: 'Internal server error' });
-    }
-    next();
-};
-exports.errorMiddleware = errorMiddleware;
+MIT License
+
+Copyright (c) Microsoft Corporation.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE
